fd835cea52ad8e576eefbe4af96b4ecd13c7fb45
jakub <jsvihla@gmail.com>
2016-06-22 14:04:01 +0100

    added test

--- test/spec/svg-spec.js
+++ test/spec/svg-spec.js
@@ -8,6 +8,12 @@ describe('Sam and Jakub tests', function(){
   it('if when a country clicked, it changes its colour', function(){
     var actual = changeColour();
     var expected = '#ff0000';
-    expect(expected).toBe(actual); 
+    expect(expected).toBe(actual);
+  })
+
+  it('if array of countries is returned', function(){
+    var actual = getCountries()[2];
+    var expected = 'AL';
+    expect(expected).toBe(actual);
   })
 })
