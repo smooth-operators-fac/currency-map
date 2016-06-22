81ada54d54fa4fd8876999a669d3f5a899c281e3
jakub <jsvihla@gmail.com>
2016-06-22 12:41:32 +0100

    testing if the colour changes

--- test/spec/svg-spec.js
+++ test/spec/svg-spec.js
@@ -4,4 +4,10 @@ describe('Sam and Jakub tests', function(){
     b = a;
     expect(b).toBe(1);
   })
+
+  it('if when a country clicked, it changes its colour', function(){
+    var actual = changeColour();
+    var expected = '#ff0000';
+    expect(expected).toBe(actual); 
+  })
 })
