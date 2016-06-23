99f68eec43628d2d2b75ab38e72e4ed508417bad
mattiaS <mattia@3doers.it>
2016-06-23 11:08:50 +0100

    added getScores method

--- js/currencyAPI.js
+++ js/currencyAPI.js
@@ -1,26 +1,94 @@
 'use strict'
 var currencyOBJ = {
-	reqType:'',
-	url:'',
-	response:null,
+	callList: [],
+	todayResponse: [],
+	response: [],
+	interval:null,
+	average:{},
+	scores:{},
 	init: function(){
+		this.interval = setInterval(this.watch.bind(currencyOBJ),10)
+		var now = new Date()
+		var dates = [5,11,18,25,32].map(el=>{
+			return now.subDays(el).format()	
+		})
+		this.callList = dates.map((el)=>{
+			return new Call(this.urls(el))	
+		})
+		var xhrArray = [0,1,2,3,4].map((el) => 'xhr' + el)
+
+		this.callList.forEach((el,i)=>{
+			xhrArray[i] = new XMLHttpRequest()	
+			xhrArray[i].onload = ()=>{
+				var clean  = new Object(JSON.parse(xhrArray[i].response).rates)
+				if (i>0) this.response.push(clean)
+				else this.todayResponse.push(clean)
+			}
+			xhrArray[i].open('GET',this.callList[i].url,true)	
+			xhrArray[i].send()	
+		})
+	},
+	urls: function(date){
+		var base = 'https://openexchangerates.org/api/';
+		var id = 'app_id=bdf1519ec8b749ff8d01851d7da1391f';
+		var historical = 'historical/'+ date + '.json';
+		return (base + historical +'?' + id)
+	},
+	reset: function(){
 		this.reqType = '';
 		this.url = '';
+		this.dates = [];		
 	},
-	set: function(reqType, url){
-		this.reqType = reqType;
-		this.url = url;
+	watch: function(){
+		if(this.response.length === 4 && this.todayResponse.length === 1){
+			this.computeAverage()
+			this.computeScores()
+			console.log(this.scores)
+			console.log(this.getScores('USD'))
+			console.log(this.getScores('AFN'))
+			clearInterval(this.interval)
+		}	
 	},
-	call: function(){
-		var xhr = new XMLHttpRequest()
+	computeAverage: function(){
+		this.average  = Object.keys(this.response[0]).reduce((acc,current)=>{
+			acc[current] = (this.response[0][current]+  this.response[1][current] +  this.response[2][current] +  this.response[3][current])/this.response.length
+			return acc
+		},{})
+	console.log(this.average)
 
-		xhr.onload = function(){
-			this.response = JSON.parse(xhr.response)
-			console.log('here: ',this.response)
-		}.bind(currencyOBJ)
-
-		xhr.open(this.reqType,this.url,true);
-		xhr.send()
+	},
+	computeScores: function(){
+		this.scores = Object.keys(this.average).reduce((acc,current)=>{
+			acc[current] =  this.todayResponse[0][current] - this.average[current]
+			return acc
+		},{}) 			
+	},
+	rebase: function(base){
+		var baseVal = this.scores[base];
+		console.log(baseVal)
+		return Object.keys(this.scores).reduce((acc,current)=>{
+			acc[current] = this.scores[current]/baseVal	
+			return acc	
+		},{})		
+	},
+	getScores: function(base){
+		if(base === 'USD')return this.scores;	
+		else return this.rebase(base)
+	
 	}
+}
 
+function Call(url){
+	this.url = url;
+}
+
+
+Date.prototype.subDays = function(days){
+	var date = new Date(this.valueOf())
+	date.setTime(date.getTime() - days*3600*24*1000)
+	return date
+}
+Date.prototype.format = function(){
+	var addZ =  (n)=>{return n<10? '0'+n:''+n;}
+	return this.getFullYear() + '-'+ addZ(this.getMonth()+1)+ '-' + addZ(this.getDate())
 }
