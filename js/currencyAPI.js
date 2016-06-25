e810427bf4aad9ac550e5e7952c035bff083bf56
mattiaS <mattia@3doers.it>
2016-06-25 17:56:38 +0100

    add promises, refactors and tries to conform more to ES6

--- js/currencyAPI.js
+++ js/currencyAPI.js
@@ -1,106 +1,86 @@
 'use strict'
-var currencyOBJ = {
-	callList: [],
-	todayResponse: [],
+
+var apiCaller = {
+	dates: null,
+	callList: null,
+	todayResponse: null,
 	response: [],
-	interval:null,
-	average:{},
-	scores:{},
-	init: function(){
-		this.interval = setInterval(this.watch.bind(currencyOBJ),10)
-		var now = new Date()
-		var dates = [0,365,700,1000,1300].map(el=>{
-			return now.subDays(el).format()
-		})
-		this.callList = dates.map((el)=>{
-			return new Call(this.urls(el))
-		})
-		var xhrArray = [0,1,2,3,4]
+	average: null,
+	scores: null,
+	runCalls(){
+		generateDates()
+		populateCallList()
+		Promise.all([callApi(this.callList[0]),callApi(this.callList[1]),
+			    callApi(this.callList[2]),callApi(this.callList[3]),
+			    callApi(this.callList[4])] )
+			    .then((responseArray)=>{
+				    for(var i = 0; i<responseArray.length; i++){
+					    if(i==0){this.todayResponse = JSON.parse(responseArray[i]).rates}
+					    else{this.response.push(JSON.parse(responseArray[i]).rates)} 	
+				    }
+				    this.runCalculations()
+			    })
 
-		this.callList.forEach((el,i)=>{
-			xhrArray[i] = new XMLHttpRequest()
-			xhrArray[i].onload = ()=>{
-				var clean  = new Object(JSON.parse(xhrArray[i].response).rates)
-				if (i>0) this.response.push(clean)
-				else this.todayResponse.push(clean)
-			}
-			xhrArray[i].open('GET',this.callList[i].url,true)
-			xhrArray[i].send()
-		})
 	},
-	urls: function(date){
-		var base = 'https://openexchangerates.org/api/';
-		var id = 'app_id=bdf1519ec8b749ff8d01851d7da1391f';
-		var historical = 'historical/'+ date + '.json';
-		return (base + historical +'?' + id)
+	runCalculations(){
+		console.log('im here!')	
+		computeAverage(this.response)
+		computeScores(this.response)
+		console.log(this.average)
+		console.log(this.scores)
 	},
-	reset: function(){
-		this.reqType = '';
-		this.url = '';
-		this.dates = [];
-	},
-	watch: function(){
-		if(this.response.length === 4 && this.todayResponse.length === 1){
-			this.computeAverage()
-			this.computeScores()
-			clearInterval(this.interval)
-		}
-	},
-	computeAverage: function(){
-		this.average  = Object.keys(this.response[0]).reduce((acc,current)=>{
-			acc[current] = (this.response[0][current]+  this.response[1][current] +  this.response[2][current] +  this.response[3][current])/this.response.length
-			return acc
-		},{})
-	},
-	computeScores: function(){
-		this.scores = Object.keys(this.average).reduce((acc,current)=>{
-			acc[current] =  (this.todayResponse[0][current] - this.average[current])/this.average[current]
-			return acc
-		},{})
-	},
-	rebase: function(base){
-		var newResponse = [];
-		this.response.forEach(function(obj, i){
-			var newObj = {}
-			Object.keys(obj).forEach(function(currency){
-				newObj[currency] = obj[currency]/obj[base]
-			})
-			newResponse.push(newObj)
-		})
-
-		var tempObj = {}
-		Object.keys(this.todayResponse[0]).forEach((currency) => {
-				tempObj[currency] = this.todayResponse[0][currency]/this.todayResponse[0][base]
-		})
-		var newTodayResponse = [tempObj]
-		var tempTodayResponse = this.todayResponse
-		this.todayResponse = newTodayResponse
-		var tempResponse = this.response
-		this.response = newResponse
-		this.computeAverage();
-		this.computeScores();
-		this.response = tempResponse
-		this.todayResponse = tempTodayResponse
-		return this.scores;
-	},
-	getScores: function(base){
-		if(base === 'USD'){
-			console.log('base: ', base, ' | ', this.scores)
-			return this.scores;
-		} else {
-			var out = this.rebase(base)
-			console.log('base: ', base, ' | ', out)
-			return out
-		}
+	getScores(base){
+		if(this.scores == null){console.log('data not ready yet'); return false}
+		else{return (base === 'USD')? this.scores: rebase(base)}
 	}
 }
 
-function Call(url){
-	this.url = url;
+function generateDates(){
+	var now = new Date()
+	apiCaller.dates = [0,365,700,1000,1300].map(el=> now.subtractDays(el).format())
 }
 
+function populateCallList(){
+	apiCaller.callList = apiCaller.dates.map((el)=> urls(el))
+}
 
-Date.prototype.subDays = function(days){
+function urls (date){
+	var base = 'https://openexchangerates.org/api/';
+	var id = 'app_id=bdf1519ec8b749ff8d01851d7da1391f';
+	var historical = 'historical/'+ date + '.json';
+	return (base + historical +'?' + id)
+}
+function callApi(url){
+	return new Promise(function (resolve, reject){
+		var request = new XMLHttpRequest();
+		request.open('GET', url, true);
+		request.onload = function (){(this.status === 200|| this.status === 304)? resolve(request.responseText): reject(request.responseText)}
+		request.send()
+	})
+}
+
+function computeAverage(){
+	apiCaller.average  = Object.keys(apiCaller.response[0]).reduce((acc,current)=>{
+		acc[current] = (apiCaller.response[0][current]+  apiCaller.response[1][current] +  apiCaller.response[2][current] +  apiCaller.response[3][current])/apiCaller.response.length
+		return acc
+	},{})
+}
+function computeScores(){
+	apiCaller.scores = Object.keys(apiCaller.average).reduce((acc,current)=>{
+		acc[current] =  (apiCaller.todayResponse[current] - apiCaller.average[current])/apiCaller.average[current]
+		return acc
+	},{})
+}
+function rebase(base){
+	var tmpObj
+	tmpObj  = Object.keys(apiCaller.average).reduce((acc,current)=>{
+		acc[current] = apiCaller.average[current]/apiCaller.average[base]
+		return acc
+	},{})
+        computeScores(tmpObj)
+	return apiCaller.scores
+}
+Date.prototype.subtractDays = function(days){
 	var date = new Date()
 	date.setTime(date.getTime() - days*3600*24*1000)
 	return date
@@ -110,4 +90,4 @@ Date.prototype.format = function(){
 	return this.getFullYear() + '-'+ addZ(this.getMonth()+1)+ '-' + addZ(this.getDate())
 }
 
-currencyOBJ.init();
+apiCaller.runCalls()
