63f995ea643b3e97e79f969f536f73ce78fad4c9
samgalson@gmail.com <samgalson@gmail.com>
2016-07-02 13:45:24 +0100

    sets up tests

---
+++ test/backend/server-test.js
@@ -0,0 +1,30 @@
+var tape = require('tape');
+var shot = require('shot');
+
+var handler = require('../../server/handler.js');
+
+//this test is from handler(make array and then load html)
+tape('GET request to / endpoint', function(t){
+  shot.inject(handler, {method: 'get', url: '/'}, function(res){
+    t.equal(res.statusCode, 200, 'responds with status 200');
+    t.ok(res.payload.includes('<!DOCTYPE'), 'responds with html file');
+    t.equal(res.headers['Content-Type'], 'text/html', 'responds with header specifying type html');
+    t.end();
+  });
+});
+
+tape('GET request to wrong url', function(t){
+  shot.inject(handler, {method: 'get', url: '/bad-url'}, function(res){
+    t.equal(res.statusCode, 404, 'responds with status 404');
+    t.end();
+  });
+});
+
+tape('GET request to /public/js/svg.js endpoint', function(t){
+	shot.inject(handler, {method: 'get', url: '/public/js/svg.js'}, function(res){
+    t.equal(res.statusCode, 200, '/ has status code of 200');
+    t.ok(res.payload.includes('selectBox'), 'responds with correct file');
+    t.end();
+  });
+});
+
